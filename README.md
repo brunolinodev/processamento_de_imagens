e649f44652cf2671fd4f4258ad362d2fb864c0b6
Lino <brunolinoeng@gmail.com>
2022-09-26 15:54:29 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# processamento_de_imagens
+
+Description. 
+The package package_name is used to:
+	Processing: 
+		-Histogram matching
+		-Structural similarity
+		-Resize image
+	Utils:
+		Read image
+		Save image
+		Plot image
+		Plotresult
+		Plot histogram
+		
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+
+```bash
+pip install processamento_de_imagens
+```
+
+## Author
+bruno_lino
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="processamento_de_imagens",
+    version="0.0.1",
+    author="bruno_lino",
+    author_email="brunolinoeng@mail.com",
+    description="Image Processing Package usin skimage",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_github_repository_project_link"
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
